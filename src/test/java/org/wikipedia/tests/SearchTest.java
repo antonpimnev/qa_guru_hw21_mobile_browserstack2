64d50bb2cd633587eddec3f0a73e50281347cf2e
Anton Pimnev <anton.pimnev@gmail.com>
2023-02-19 14:10:37 +0500

    new tests

--- src/test/java/org/wikipedia/tests/SearchTest.java
+++ src/test/java/org/wikipedia/tests/SearchTest.java
@@ -4,21 +4,43 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.id;
 
 public class SearchTest extends TestBase {
 
     @Test
-    @DisplayName("Поиск статей")
+    @DisplayName("Проверка функции поиска")
     void searchTest() {
-        //back();
         step("Type search", () -> {
-            $(id("org.wikipedia:id/search_container")).click();
-            $(id("org.wikipedia:id/search_src_text")).setValue("BrowserStack");
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
         });
         step("Verify content found", () ->
-                $$(id("org.wikipedia:id/page_list_item_title")).shouldHave(sizeGreaterThan(0)));
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("Проверка перехода к главному экрану из статьи")
+    void goToTheArticleTest() {
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Selenium");
+        });
+        step("Click on the selenium article", () -> {
+            $(id("org.wikipedia.alpha:id/fragment_feed_header")).click();
+        });
+        step("Click on the back button", () -> {
+            $(id("org.wikipedia.alpha:id/view_wiki_error_button")).click();
+        });
+        step("Check visibility of header logo", () ->
+                $(id("org.wikipedia.alpha:id/single_fragment_toolbar_wordmark"))
+                        .shouldBe(visible));
+
     }
 }
